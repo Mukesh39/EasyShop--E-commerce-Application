0ee7984572e028f3271a8d871cc8a7a372f78b2c
Mukesh Kumar <muk.786422@gmail.com>
2022-08-13 17:16:51 +0530

    UI made With all basic Functions

---
+++ src/redux/action.js
@@ -0,0 +1,5 @@
+export const ADD_TO_CART = 'ADD_TO_CARD' ;
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const LOAD_CURRENT_ITEM =  'LOAD_CURRENT_ITEM';
+export const UPDATE_QTY = 'UPDATE_QTY';
+
